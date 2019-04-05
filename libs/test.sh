78bb4780816d112b4f373a79397bde98d06878e7
Hans Kristian Moen <hans.kristian.moen@gmail.com>
2019-04-05 17:39:01 +0100

    delete local test file

--- libs/test.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-source icp-kube-functions.bash
-#_skip_destroy="true"
-if [[ $(type -t run_as) && ! "$_skip_destroy" == "true" ]]; then
-      echo "DESTROY"
-    fi
-#run_as -u me -n myself echo "foo bar baz -n someone"
-#echo $?
