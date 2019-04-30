ad5b395711a7dad9587f71215b3f870f947b97d5
Hans Kristian Moen <hans.kristian.moen@gmail.com>
2019-04-30 23:49:20 +0100

    add additional compats

--- libs/sert-compat.bash
+++ libs/sert-compat.bash
@@ -9,3 +9,15 @@ function get_num_master() {
   # the $(( N - 1)) instead of kubectl --no-headers deals with wc -l putting in a ton of spaces
   export node_number=$(( $(kube get nodes -l master=true | wc -l) - 1 ))
 }
+
+function get_master_ip() {
+  export master_ip=${SERVER}
+}
+
+function get_router_https_port() {
+  export router_https_port=${ROUTER_HTTPS_PORT}
+}
+
+function get_auth_token() {
+  export auth_token=$(kube config view -o jsonpath='{.users[?(@.name=="admin")].user.token}')
+}
